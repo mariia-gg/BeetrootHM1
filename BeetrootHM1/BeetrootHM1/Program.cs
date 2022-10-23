04a24adaec3443cba2aa5dadfe0a651739d36c4e
mariia-gg <superkomarnitska@gmail.com>
2022-10-23 09:57:15 +0300

    Beggin code

---
+++ BeetrootHM1/BeetrootHM1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grn = 40;
+            Console.WriteLine(" Введiть долар: ");
+            var dollar = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("dollar " + dollar);
+            Console.WriteLine("Долари переведенi в гривнi ");
+            Console.WriteLine("Grn " + dollar * grn);
+
+        }
+    }
+}
